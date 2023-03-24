774b3238cc257557743087c72bd704aca3be36c2
Chrisfelix162 <44751449+Chrisfelix162@users.noreply.github.com>
2023-03-24 15:47:05 -0700

    Update get-hybridserviceurl.ps1
    Signed-off-by: Chrisfelix162 <44751449+Chrisfelix162@users.noreply.github.com>

--- Powershell/get-hybridserviceurl.ps1
+++ Powershell/get-hybridserviceurl.ps1
@@ -1,6 +1,6 @@
 $token = (Get-AzAccessToken).Token
-$ac = "Aptify70auto"
-$url = "https://eastus.management.azure.com/subscriptions/fd7747d0-6fa3-4604-8810-848b5aa126ca/resourcegroups/Aptify70POC/providers/Microsoft.Automation/automationAccounts/${ac}?api-version=2021-06-22"
+$ac = "AutomationAccount"
+$url = "https://eastus.management.azure.com/subscriptions/fd7747d0-6fa3-4604-8810-848b5aa126ca/resourcegroups/$RG/providers/Microsoft.Automation/automationAccounts/${ac}?api-version=2021-06-22"
 $data = Invoke-RestMethod -Method Get $url -Headers @{"Authorization" = "Bearer $token"}
 $automationHybridServiceUrl = $data.properties.automationHybridServiceUrl
 $automationHybridServiceUrl
